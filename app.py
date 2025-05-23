a083e2f5c46450aa64ce36ef569dc73b7a58f599
John Ho <jho.yvr+git@gmail.com>
2025-05-23 09:57:13 -0400

    added point and object_detection features

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import gradio as gr
 import spaces, torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from PIL import Image
+from typing import Literal
 
 
 @spaces.GPU
@@ -15,17 +16,23 @@ def load_model():
 
 
 @spaces.GPU
-def point(im: Image.Image, object_name: str):
+def detect(
+    im: Image.Image, object_name: str, mode: Literal["point", "object_detection"]
+):
     model = load_model()
-    return model.detect(im, object_name)["objects"]
+    if mode == "point":
+        return model.point(im, object_name)["points"]
+    elif mode == "object_detection":
+        return model.detect(im, object_name)["objects"]
 
 
 demo = gr.Interface(
-    fn=point,
+    fn=detect,
     inputs=[
         gr.Image(label="Input Image", type="pil"),
         gr.Textbox(label="Object to Detect"),
+        gr.Dropdown(label="Mode", choices=["point", "object_detection"]),
     ],
     outputs=gr.Textbox(label="Output Text"),
 )
-demo.launch()
+demo.launch(mcp_server=True)
