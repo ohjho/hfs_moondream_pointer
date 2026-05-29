1e1ee77261cad4d33ab68654e29842fe6401bab8
John Ho <jho.yvr+git@gmail.com>
2026-05-29 08:46:42 -0400

    revert back to moondream2

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ from typing import Literal
 
 @spaces.GPU(duration=60)
 def load_model():
-    # moondream2
-    # return AutoModelForCausalLM.from_pretrained(
-    #     "vikhyatk/moondream2",
-    #     revision="2025-04-14",
-    #     trust_remote_code=True,
-    #     device_map={"": "cuda"},
-    # )
-
-    # moondream3-preview
-    moondream = AutoModelForCausalLM.from_pretrained(
-        "moondream/moondream3-preview",
+    moondream2
+    return AutoModelForCausalLM.from_pretrained(
+        "vikhyatk/moondream2",
+        revision="2025-04-14",
         trust_remote_code=True,
-        dtype=torch.bfloat16,
         device_map={"": "cuda"},
     )
-    moondream.compile()
-    return moondream
+
+    # moondream3-preview
+    # moondream = AutoModelForCausalLM.from_pretrained(
+    #     "moondream/moondream3-preview",
+    #     trust_remote_code=True,
+    #     dtype=torch.bfloat16,
+    #     device_map={"": "cuda"},
+    # )
+    # moondream.compile()
+    # return moondream
 
 
 _MODEL = load_model()
